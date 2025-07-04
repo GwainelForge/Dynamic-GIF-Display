87576a7c5770c509cf59a5eb5043e5eb8378e10f
GwainelForge <gwainel13@gmail.com>
2025-07-04 14:07:12 +0200

    Add files via upload

---
+++ viewer.js
@@ -0,0 +1,10 @@
+﻿Twitch.ext.onAuthorized(() => {
+    Twitch.ext.configuration.onChanged(() => {
+        if (Twitch.ext.configuration.broadcaster) {
+            const gifUrl = JSON.parse(Twitch.ext.configuration.broadcaster.content).gifUrl;
+            if (gifUrl) {
+                document.getElementById("gif").src = gifUrl;
+            }
+        }
+    });
+});
