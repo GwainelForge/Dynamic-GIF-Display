12de42dd0009f6df3969a659499fbfa0d2646e42
GwainelForge <gwainel13@gmail.com>
2025-07-04 14:24:25 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
-﻿function saveConfig() {
-    const gifUrl = document.getElementById("gifUrl").value;
-    Twitch.ext.configuration.set("broadcaster", "1", JSON.stringify({ gifUrl }));
+function saveConfig() {
+  const gifUrl = document.getElementById("gifUrl").value;
+  Twitch.ext.configuration.set("broadcaster", "1", JSON.stringify({ gifUrl }));
 }
 
 Twitch.ext.onAuthorized(() => {
-    Twitch.ext.configuration.onChanged(() => {
-        if (Twitch.ext.configuration.broadcaster) {
-            const config = JSON.parse(Twitch.ext.configuration.broadcaster.content);
-            document.getElementById("gifUrl").value = config.gifUrl || "";
-        }
-    });
+  Twitch.ext.configuration.onChanged(() => {
+    if (Twitch.ext.configuration.broadcaster) {
+      const config = JSON.parse(Twitch.ext.configuration.broadcaster.content);
+      document.getElementById("gifUrl").value = config.gifUrl || "";
+    }
+  });
 });
